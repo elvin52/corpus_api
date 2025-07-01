b86b811d04c1921a12a7a0041fe6f2cafd3ae713
elvin52 <80586034+elvin52@users.noreply.github.com>
2025-07-01 16:20:16 +0200

    Create Launcher.java

---
+++ javadoc/org/jqurantree/Launcher.java
@@ -0,0 +1,7 @@
+package org.quran.tree;
+public class Launcher {
+    public static void main(String[] args) {
+        // Call your library's API here
+        System.out.println("API is running!");
+    }
+}
